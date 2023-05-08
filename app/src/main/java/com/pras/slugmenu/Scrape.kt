1a17f5083bbe2717d80b00b4192eb374743f5f5a
Prasiddh <prapooskur@gmail.com>
2023-05-08 11:56:09 -0700

    removed okhttp and retrofit, cleaned up scrape

--- app/src/main/java/com/pras/slugmenu/Scrape.kt
+++ app/src/main/java/com/pras/slugmenu/Scrape.kt
@@ -13,14 +13,6 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.cookies.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import retrofit2.Retrofit
-import java.io.IOException
-import java.time.Duration
-import java.time.LocalDateTime
-import java.time.LocalTime
-
 
 /*
 CATEGORIES:
@@ -28,77 +20,6 @@ CATEGORIES:
 - getSingleMenu: Perks, Terra Fresca, Stevenson, Porter, Global Village
 - getOakesMenu: Oakes
  */
-
-/*
-suspend fun scrapeWebDataRetrofit (inputUrl: String): String {
-    val baseurl = "https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum="
-    val url: String = baseurl+inputUrl
-    val locationCookie: String = inputUrl.substring(0,2)
-
-    //extremely scuffed cache time solution
-    val now = LocalDateTime.now()
-    val endOfDay = now.with(LocalTime.MAX)
-    val duration = Duration.between(now, endOfDay)
-    val secondsLeft: Int = duration.seconds.toInt()
-
-    Log.d("TAG", "Seconds left in day: $secondsLeft")
-
-
-    val client = Retrofit.Builder()
-        .baseUrl(url)
-        .
-        .build();
-
-    var html = ""
-    try {
-        val response = client.newCall(request).execute()
-        html = response.body?.string().toString()
-    } catch (e: IOException) {
-        Log.d("TAG", "Exception while scraping: $e")
-    }
-    Log.d("TAG", "HTML: $html")
-    return html
-}
- */
-
-//cookies currently don't work on this
-suspend fun scrapeWebDataOkHTTP (inputUrl: String): String {
-    val baseurl = "https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum="
-    val url: String = baseurl+inputUrl
-    val locationCookie: String = inputUrl.substring(0,2)
-
-    //extremely scuffed cache time solution
-    val now = LocalDateTime.now()
-    val endOfDay = now.with(LocalTime.MAX)
-    val duration = Duration.between(now, endOfDay)
-    val secondsLeft: Int = duration.seconds.toInt()
-
-    Log.d("TAG", "Seconds left in day: $secondsLeft")
-
-
-    val client = OkHttpClient()
-
-    val request = Request.Builder()
-        .url(url)
-        .header("Cookie", "WebInaCartDates")
-        .header("Cookie", "WebInaCartLocation=$locationCookie")
-        .header("Cookie", "WebInaCartMeals")
-        .header("Cookie", "WebInaCartQtys")
-        .header("Cookie", "WebInaCartRecipes")
-        .header("Cache-Control", "max-age=$secondsLeft")
-        .build()
-
-    var html = ""
-    try {
-        val response = client.newCall(request).execute()
-        html = response.body?.string().toString()
-    } catch (e: IOException) {
-        Log.d("TAG", "Exception while scraping: $e")
-    }
-    Log.d("TAG", "HTML: $html")
-    return html
-}
-
 
 suspend fun scrapeWebData (inputUrl: String): String {
     val baseurl = "https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum="
@@ -154,8 +75,8 @@ suspend fun getWebData (inputUrl: String): MutableList<MutableList<String>> {
             val trs: Elements = rows.select("tr")
 
             for (j in trs) {
-                var separators: String = j.select("span[style=\"color: #000000\"]").toString()
-                var items: String = j.select("span[style=\"color: #585858\"]").toString()
+                val separators: String = j.select("span[style=\"color: #000000\"]").toString()
+                val items: String = j.select("span[style=\"color: #585858\"]").toString()
                 if (separators.length > 29 && !separators.contains("&nbsp;")) {
                     var cleanSeparator = separators.substring(29, separators.length - 7)
                     cleanSeparator = cleanSeparator.replace("&amp;", "&")
@@ -172,7 +93,7 @@ suspend fun getWebData (inputUrl: String): MutableList<MutableList<String>> {
                     var cleanItem = items.substring(29, items.length - 13)
                     cleanItem = cleanItem.replace("&amp;", "&")
                     if (!listItems.contains(cleanItem)) {
-                        Log.d("TAG", "clean item: "+cleanItem)
+                        Log.d("TAG", "clean item: $cleanItem")
                         listItems.add(cleanItem)
                     }
                 }
