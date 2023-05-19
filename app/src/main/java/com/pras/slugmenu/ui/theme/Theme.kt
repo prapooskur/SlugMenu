e5cb942bba8ebb8bfa4124e5583b58f0b9c86ddf
Prasiddh <prapooskur@gmail.com>
2023-05-19 00:37:34 -0700

    updated status bar color

--- app/src/main/java/com/pras/slugmenu/ui/theme/Theme.kt
+++ app/src/main/java/com/pras/slugmenu/ui/theme/Theme.kt
@@ -115,7 +115,7 @@ fun SlugMenuTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.primaryContainer.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
