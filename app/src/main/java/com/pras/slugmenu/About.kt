44806f461c7a9f941cdc583fe6ed166c9f44086d
Prasiddh <prapooskur@gmail.com>
2023-06-15 22:04:17 -0700

    updated intents in about menu

--- app/src/main/java/com/pras/slugmenu/About.kt
+++ app/src/main/java/com/pras/slugmenu/About.kt
@@ -3,18 +3,15 @@ package com.pras.slugmenu
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -27,20 +24,18 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
-@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
+private const val TAG = "About"
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen(navController: NavController, preferencesDataStore: PreferencesDatastore) {
     val useCollapsingTopBar = remember { mutableStateOf(false) }
@@ -62,6 +57,7 @@ fun AboutScreen(navController: NavController, preferencesDataStore: PreferencesD
     }
 
     val appVersion = BuildConfig.VERSION_NAME
+    val context = LocalContext.current
 
     Scaffold(
         modifier = scaffoldModifier,
@@ -82,7 +78,10 @@ fun AboutScreen(navController: NavController, preferencesDataStore: PreferencesD
                     AboutItem(appVersion = appVersion)
                 }
                 item {
-                    GithubItem(context = LocalContext.current)
+                    GithubItem(context = context)
+                }
+                item {
+                    ContactItem(context = context)
                 }
             }
         }
@@ -122,9 +121,39 @@ fun GithubItem(context: Context) {
         headlineContent = { Text(text = "Source Code") },
         supportingContent = { Text(text = "Released under the GNU General Public License v3.0") },
         modifier = Modifier.clickable {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://github.com/prapooskur/SlugMenu")
-            ContextCompat.startActivity(context, intent, null)
+            val webpage: Uri = Uri.parse("https://github.com/prapooskur/SlugMenu")
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            context.startActivity(intent)
+        }
+    )
+}
+
+@Composable
+fun ContactItem(context: Context) {
+//    val packageManager = context.packageManager
+    ListItem(
+        leadingContent = {
+            // add email icon here
+            Icon(
+                Icons.Outlined.Email,
+                contentDescription = "Mail",
+                modifier = Modifier.size(24.dp)
+            )
+        },
+        headlineContent = { Text(text = "Contact") },
+        supportingContent = { Text(text = "Send suggestions, feedback, etc to slugmenudev@gmail.com") },
+        modifier = Modifier.clickable {
+            val mail = Uri.parse("mailto:slugmenudev@gmail.com")
+            val intent = Intent(Intent.ACTION_SENDTO, mail)
+            context.startActivity(intent)
+            /*
+            if (intent.resolveActivity(packageManager) != null) {
+                context.startActivity(intent)
+            } else {
+                Toast.makeText(context, "No email app found", Toast.LENGTH_SHORT).show()
+            }
+             */
+
         }
     )
 }
