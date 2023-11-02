23a3611d452b9d75f8cac8c700b5b93d21bc0e39
prapooskur <61389919+prapooskur@users.noreply.github.com>
2023-11-01 23:50:49 -0700

    bumped version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.pras.slugmenu"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.2.4"
+        versionCode = 16
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
