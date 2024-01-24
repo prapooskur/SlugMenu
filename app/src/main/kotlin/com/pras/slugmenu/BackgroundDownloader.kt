03836285a3a84c8f2f0c6e6e1cfa5cbe04453e58
prapooskur <>
2024-01-24 13:53:55 -0800

    time zone and notification updates

--- app/src/main/kotlin/com/pras/slugmenu/BackgroundDownloader.kt
+++ app/src/main/kotlin/com/pras/slugmenu/BackgroundDownloader.kt
@@ -36,6 +36,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.util.concurrent.TimeUnit
 
 private const val TAG = "BackgroundDownloadWorker"
@@ -91,6 +92,8 @@ class BackgroundDownloadWorker(context: Context, params: WorkerParameters): Coro
                     val notifyFavorites = isPersistent && notifyPreference && notifyPermissionGranted
 
                     val favoritesMap = mutableMapOf<String, FavoriteItem>()
+                    Log.d(TAG, favoritesDao.getFavorites().toString())
+                    val favoritesExist = favoritesDao.getFavorites().isNotEmpty()
 
                     if (notifyFavorites) {
                         Log.d(TAG, "Notifications enabled")
@@ -116,7 +119,7 @@ class BackgroundDownloadWorker(context: Context, params: WorkerParameters): Coro
                                 if (menuList.isNotEmpty()) {
                                     // send notifications if user has requested it, making sure to check if permissions were granted
                                     // should this only notify for background downloads, or for both background and user-requested ones?
-                                    if (notifyFavorites) {
+                                    if (notifyFavorites && favoritesExist) {
                                         // queue up favorited item notifications
                                         for (menu in menuList.indices) {
                                             val favoritesList = favoritesDao.selectFavorites(menuList[menu].toSet())
@@ -137,7 +140,6 @@ class BackgroundDownloadWorker(context: Context, params: WorkerParameters): Coro
                                                     .add(time)
                                             }
                                         }
-
                                     }
 
                                     menuDao.insertMenu(
@@ -157,89 +159,93 @@ class BackgroundDownloadWorker(context: Context, params: WorkerParameters): Coro
                     }
                     deferredResults.awaitAll()
 
-                    if (notifyFavorites) {
+                    if (notifyFavorites && favoritesExist) {
                         val groupKey = "com.pras.slugmenu.FAVORITE_NOTIFICATION"
                         val childNotificationList = mutableListOf<NotificationCompat.Builder>()
+                        Log.d(TAG, favoritesMap.toString())
                         val favorites = favoritesMap.values
                         Log.d(TAG, favorites.toString())
-                        for (favorite in favorites) {
-                            val locValues = favorite.locations.toList()
-                            val timesList = mutableListOf<String>()
-                            for (value in locValues) {
-                                val times = when (value.second.size) {
-                                    1 -> value.second[0]
-                                    2 -> value.second.joinToString(" and ")
-                                    else -> value.second.subList(0, value.second.size - 1)
-                                        .joinToString(", ") + ", and " + value.second.last()
+                        if (favorites.isNotEmpty()) {
+                            for (favorite in favorites) {
+                                val locValues = favorite.locations.toList()
+                                val timesList = mutableListOf<String>()
+                                for (value in locValues) {
+                                    val times = when (value.second.size) {
+                                        1 -> value.second[0]
+                                        2 -> value.second.joinToString(" and ")
+                                        else -> value.second.subList(0, value.second.size - 1)
+                                            .joinToString(", ") + ", and " + value.second.last()
+                                    }
+                                    timesList.add("at ${value.first.replace("Stevenson","Stev")} for $times")
                                 }
-                                timesList.add("at ${value.first.replace("Stevenson","Stev")} for $times")
-                            }
-                            timesList.sortBy {
-                                when (it.substringAfter("at ").substringBefore(" for ")) {
-                                    "Nine/Lewis" -> 1
-                                    "Cowell/Stevenson" -> 2
-                                    "Cowell/Stev" -> 2
-                                    "Crown/Merrill" -> 3
-                                    "Porter/Kresge" -> 4
-                                    "Carson/Oakes" -> 5
-                                    else -> 6
+                                timesList.sortBy {
+                                    when (it.substringAfter("at ").substringBefore(" for ")) {
+                                        "Nine/Lewis" -> 1
+                                        "Cowell/Stevenson" -> 2
+                                        "Cowell/Stev" -> 2
+                                        "Crown/Merrill" -> 3
+                                        "Porter/Kresge" -> 4
+                                        "Carson/Oakes" -> 5
+                                        else -> 6
+                                    }
                                 }
+
+                                val sortKey = favorite.name
+
+                                val bigTextStyle = NotificationCompat.BigTextStyle().bigText(timesList.joinToString("\n"))
+
+
+                                val contentText = if (timesList.size == 1) { timesList[0] } else { "At ${timesList.size} locations today" }
+
+                                val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                                    .setSmallIcon(R.drawable.slugicon_notification_foreground)
+                                    .setContentTitle(favorite.name)
+                                    .setContentText(contentText)
+                                    .setStyle(bigTextStyle)
+                                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                                    .setGroup(groupKey)
+                                    .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_SUMMARY)
+                                    .setSortKey(sortKey)
+                                    .setAutoCancel(true)
+
+                                childNotificationList.add(builder)
                             }
 
-                            val sortKey = favorite.name
-
-                            /*
-                            val inboxStyle = NotificationCompat.InboxStyle()
-                            Log.d(TAG, timesList.joinToString("\n"))
-                            for (time in timesList) {
-                                inboxStyle.addLine(time)
-                            }
-                             */
-                            val bigTextStyle = NotificationCompat.BigTextStyle().bigText(timesList.joinToString("\n"))
-
-
-                            val contentText = if (timesList.size == 1) { timesList[0] } else { "At ${timesList.size} locations today" }
-
-                            val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                            //todo improve appearance on older devices
+                            val summaryNotification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                                .setContentTitle("${favorites.size} favorited items available")
+                                // Set content text to support devices running API level < 24.
+                                // minapi is 24, does this matter?
+                                .setContentText(favorites.joinToString { "${it.name}\n" })
                                 .setSmallIcon(R.drawable.slugicon_notification_foreground)
-                                .setContentTitle(favorite.name)
-                                .setContentText(contentText)
-                                .setStyle(bigTextStyle)
-                                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                                // Build summary info into InboxStyle template.
+                                .setStyle(NotificationCompat.InboxStyle())
+                                // Specify which group this notification belongs to.
                                 .setGroup(groupKey)
-                                .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_SUMMARY)
-                                .setSortKey(sortKey)
-                                .setAutoCancel(true)
-
-                            childNotificationList.add(builder)
-                        }
-
-                        //todo improve appearance on older devices
-                        val summaryNotification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
-                            .setContentTitle("Favorited items available")
-                            // Set content text to support devices running API level < 24.
-                            .setContentText("Favorited items available")
-                            .setSmallIcon(R.drawable.slugicon_notification_foreground)
-                            // Build summary info into InboxStyle template.
-                            .setStyle(NotificationCompat.InboxStyle())
-                            // Specify which group this notification belongs to.
-                            .setGroup(groupKey)
-                            // Set this notification as the summary for the group.
-                            .setGroupSummary(true)
-                            .build()
+                                // Set this notification as the summary for the group.
+                                .setGroupSummary(true)
+                                .build()
 
 
-                        with(NotificationManagerCompat.from(applicationContext)) {
-                            for (builder in childNotificationList) {
-                                // notificationId is a unique int for each notification that you must define.
-                                val oneTimeID = System.currentTimeMillis()
-                                notify(oneTimeID.toInt(), builder.build())
-                                Log.d(TAG,"sending child notification")
-                                // small delay to avoid notifications not showing
-                                delay(50)
+                            with(NotificationManagerCompat.from(applicationContext)) {
+                                for (builder in childNotificationList) {
+                                    // notificationId is a unique int for each notification that you must define.
+                                    val oneTimeID = System.currentTimeMillis()
+                                    notify(oneTimeID.toInt(), builder.build())
+                                    Log.d(TAG,"sending child notification")
+                                    // small delay to avoid notifications not showing
+                                    delay(50)
+                                }
+                                // The summary notification requires a constant ID.
+                                notify(0, summaryNotification)
                             }
-                            // The summary notification requires a constant ID.
-                            notify(0, summaryNotification)
+                        } else {
+                            Log.d(TAG, "favorites empty, skipping notifications")
+                        }
+                    } else {
+                        when {
+                            !notifyFavorites -> Log.d(TAG, "Notifications disabled, skipping")
+                            !favoritesExist -> Log.d(TAG, "No favorites, skipping notifications")
                         }
                     }
 
@@ -322,20 +328,34 @@ object BackgroundDownloadScheduler {
     fun refreshPeriodicWork(context: Context) {
         // hardcoded to PST, since that's where UCSC is
         val timeZone = ZoneId.of("America/Los_Angeles")
+        Log.d(TAG, "Timezone is $timeZone")
 
-        val currentDateTime = LocalDateTime.now()
+        val currentDateTime = LocalDateTime.now(timeZone)
+
+        if (currentDateTime.hour !in 0..2) {
+            Log.d(TAG, "Work started outside the flex interval.")
+            //throw Exception("Work started outside the flex interval.")
+        }
 
         // set to 2AM because workmanager may not download at the exact time
+        /*
         var executionDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(2, 0))
             .atZone(timeZone)
             .toLocalDateTime()
+         */
+        var executionDateTime = ZonedDateTime.of(LocalDate.now(), LocalTime.of(2, 0), timeZone)
+            .withZoneSameInstant(ZoneId.systemDefault())
+            .toLocalDateTime()
 
+        Log.d(TAG, "Current time is $currentDateTime")
+        Log.d(TAG, "Execution time is $executionDateTime")
         if (executionDateTime.isBefore(currentDateTime)) {
             Log.d(TAG,"current time is after 2:00 AM, scheduling for tomorrow")
+            Log.d(TAG, "Execution time is $executionDateTime")
             executionDateTime = executionDateTime.plusDays(1)
         }
 
-        Log.d(TAG, "Scheduled for $executionDateTime")
+        //Log.d(TAG, "Scheduled for $executionDateTime")
 
 
         val duration = Duration.between(currentDateTime, executionDateTime)
